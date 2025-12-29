ebd80a62ac36d152699e7b0a93f2bb5fb79fa4a5
shuklasachinaxis-cloud <shuklasachinaxis@gmail.com>
2025-12-29 10:42:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from sklearn.ensemble import RandomForestRegressor
 import streamlit as st
 import pickle
 from sklearn.preprocessing import StandardScaler
@@ -20,8 +21,29 @@ st.sidebar.title('Select House features: ')
 st.sidebar.image('https://m.media-amazon.com/images/I/71ea+0NVViL.jpg')
 all_value = []
 for i in final_X:
+  min_val = final_X[i].min()
+  max_value = final_X[i].max()
   result = st.sidebar.slider(f'select {i} value')
   all_value.append(result)
 
-st.write(all_value)
+user_X = scaler.transform([all_value])
+
+@st.cache_data
+def ml_model(x,y):
+  model = randomforestregressor()
+  model.fit(X,y)
+  return model
+
+model = ml_model(scaled_X,y)
+house_price = model.predict(user_X)[0]
+
+final_price = round(house_price * 100000,2)
+
+with st.spinner("predicting house price"):
+  import time
+  time.sleep(2)
+
+st.success(f'estimated house price is : ${final_price}")
+st.markdown('''**Design and development by: Mr Sachin Shukla**''')
+           
 
